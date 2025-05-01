194fa89ad304b316c505520c1d0a6036ee7db55e
Tom Miller <tommymillerr@outlook.com>
2025-05-01 19:52:58 -0400

    Add today LC problems command

--- src/start-bot.ts
+++ src/start-bot.ts
@@ -3,7 +3,7 @@ import { Options, Partials } from 'discord.js';
 import { createRequire } from 'node:module';
 
 import { Button } from './buttons/index.js';
-import { DevCommand, HelpCommand, InfoCommand, TestCommand } from './commands/chat/index.js';
+import { DevCommand, HelpCommand, InfoCommand, TodayCommand, TestCommand } from './commands/chat/index.js';
 import {
     ChatCommandMetadata,
     Command,
@@ -80,6 +80,7 @@ async function start(): Promise<void> {
         new ViewDateJoined(),
 
         // TODO: Add new commands here
+        new TodayCommand(),
     ];
 
     // Buttons
